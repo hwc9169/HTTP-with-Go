93bf0fa0fd1763c5f5545bdeb2a373b09d70dd65
조호원 <hwc9169@gmail.com>
2021-01-28 14:39:26 +0900

    Notice first visiting with cookie

--- HTTP-with-Go/server.go
+++ HTTP-with-Go/server.go
@@ -8,19 +8,25 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Set-Cookie", "VISIT=TRUE")
+	if _, ok := r.Header["Cookie"]; ok {
+		//쿠키가 있기 때문에 한 번 다녀간 적이 있다.
+		fmt.Fprintf(w, "<html><body>다녀간 적 있음</body></html>\n")
+	} else {
+		fmt.Fprintf(w, "<html><body>첫방문</body></html>\n")
+	}
 	dump, err := httputil.DumpRequest(r, true)
 	if err != nil {
 		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
 		return
 	}
 	fmt.Println(string(dump))
-	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
 }
 
 func main() {
 	var httpServer http.Server
 	http.HandleFunc("/", handler)
 	log.Println("start http listening : 18888")
-	httpServer.Addr = ":18888"
+	httpServer.Addr = ":5000"
 	log.Println(httpServer.ListenAndServe())
 }
