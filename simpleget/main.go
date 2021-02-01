0c42517ff31f743716b92f18a8a8c8f6e95f48c3
조호원 <hwc9169@gmail.com>
2021-02-01 10:57:30 +0900

    curl -G --data-urlencode "query=hello world"

--- simpleget/main.go
+++ simpleget/main.go
@@ -4,17 +4,16 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	resp, err := http.Get("http://127.0.0.1:5000")
-	if err != nil {
-		panic(err)
+	values := url.Values{
+		"query": {"hello world"},
 	}
+
+	resp, _ := http.Get("http://127.0.0.1:5000" + "?" + values.Encode())
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
+	body, _ := ioutil.ReadAll(resp.Body)
 	log.Println(string(body))
 }
