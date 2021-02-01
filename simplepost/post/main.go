f9db27f023fdc66e7e43022402746a3d6bae8342
조호원 <hwc9169@gmail.com>
2021-02-01 11:27:16 +0900

    프로그램 문자열 post 전송

--- simplepost/post/main.go
+++ simplepost/post/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -11,10 +12,20 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
+	// 파일을 post로 전달
 	resp, err := http.Post("http://127.0.0.1:5000", "test/plain", file)
 	if err != nil {
 		panic(err)
 	}
 	log.Println("Status: ", resp.Status)
+
+	//"문자열"을 post 전달
+	var str string = "텍스트"
+	var reader *strings.Reader = strings.NewReader(str)
+	resp, err = http.Post("http://127.0.0.1:5000", "text/plain", reader)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Status: ", resp.Status)
+
 }
