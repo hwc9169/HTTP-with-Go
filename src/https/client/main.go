1e089448505906a4179f19fea6c6a2a0bc999129
hwc9169 <hwc9169@gmail.com>
2021-02-11 23:51:31 +0900

    https client

--- src/https/client/main.go
+++ src/https/client/main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
 )
 
 func main() {
-	resp, err := http.Get("https://localhost:5050")
+	// 인증서를 읽어 드림
+	cert, err := ioutil.ReadFile("/home/hwc9169/https/ca.crt")
+	if err != nil {
+		panic(err)
+	}
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(cert)
+	tlsConfig := &tls.Config{
+		RootCAs: certPool,
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	// 클라이언트 작성
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+	// 통신한다
+	resp, err := client.Get("https://localhost:5050")
 	if err != nil {
 		panic(err)
 	}
